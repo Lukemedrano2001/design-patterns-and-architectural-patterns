fe0f4cd9fd654ff95024606b2598e55e38dbc085
Lukemedrano2001 <144495981+Lukemedrano2001@users.noreply.github.com>
2024-02-06 18:09:23 -0300

    Update IAprovaExame.java

--- src/solid/ocp/IAprovaExame.java
+++ src/solid/ocp/IAprovaExame.java
@@ -1,6 +1,6 @@
 package solid.ocp;
 
 public interface IAprovaExame {
-	void aprovarSolicitacaoExame(Exame exame);
+    void aprovarSolicitacaoExame(Exame exame);
     boolean verificaCondicoesExame(Exame exame);
 }
