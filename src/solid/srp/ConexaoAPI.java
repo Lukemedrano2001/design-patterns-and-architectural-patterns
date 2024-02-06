b52bf64b08ea3da198f0bff2d37a17ed544824e6
Lukemedrano2001 <144495981+Lukemedrano2001@users.noreply.github.com>
2024-02-06 18:10:46 -0300

    Update ConexaoAPI.java

--- src/solid/srp/ConexaoAPI.java
+++ src/solid/srp/ConexaoAPI.java
@@ -4,6 +4,6 @@ public class ConexaoAPI {
 	// A classe ConexaoAPI é responsável apenas por realizar a conexão na API
 	
 	public String getConexaoAPI(){
-        return "";
+        	return "";
     }
 }
